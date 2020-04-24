c8c3e212f069b11651743c6ea4fedb86ed4d74de
Samuel BELONDRADE <samuelbelondrade@outlook.fr>
2020-04-25 00:13:58 +0200

    start manage end line

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: sbelondr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 09:38:13 by sbelondr          #+#    #+#             */
-/*   Updated: 2020-04-23 23:42:33 by sbelondr         ###   ########.fr       */ 
+/*   Updated: 2020-04-24 23:21:27 by sbelondr         ###   ########.fr       */ 
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,18 @@ typedef struct	s_save_select
 	size_t		size_lst;
 }								t_save_select;
 
+typedef struct	s_term_parameter
+{
+	size_t					column;
+	size_t					nb_column;
+	size_t					column_max;
+	size_t					line;
+	size_t					line_max;
+	size_t					column_max_last_line;
+	t_save_select		*select;
+	struct termios	base_term;
+	int							fd_in;
+}								t_term_parameter;
 
 t_save_select	*create_lst_select(char **src);
 void				del_select(t_save_select *s);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sbelondr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 16:47:29 by sbelondr          #+#    #+#             */
-/*   Updated: 2020-04-23 23:51:37 by sbelondr         ###   ########.fr       */ 
+/*   Updated: 2020-04-25 00:12:50 by sbelondr         ###   ########.fr       */ 
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,21 @@
  ** termios.truc |= (); -> active flag
  ** termios.truc &= ~(); -> desactive flag
  */
+
+size_t	ft_size_lst_select(t_save_select *sv)
+{
+	t_select	*s;
+	size_t		sz;
+
+	s = sv->head;
+	sz = 0;
+	while (s)
+	{
+		++sz;
+		s = s->next;
+	}
+	return (sz);
+}
 
 int		ft_tty_raw(struct termios base_term, int fd_in)
 {
@@ -61,37 +76,163 @@ int	ft_pchar(int c)
 	return (c);
 }
 
-char	*test(char **list_arg)
-{
-	int		i;
-	int		j;
-	char	*keys[10];
-	char	*final;
-	char	*term;
-	char	buf[3];
 
-	term = getenv("TERM");
-	if ((!term) || tgetent(0, term) == -1)
+void	fill_screen(t_save_select *sv, t_term_parameter *term)
+{
+	size_t	current_column;
+//	int			line;
+	size_t	sz_name;
+
+//	line = tgetnum("li");
+	current_column = 0;
+//	ft_dprintf(5, "%d - %d\n", term->column, term->column_max);
+	while (sv->current)
 	{
-		ft_dprintf(2, "Error: tgetent doesn't work!\n");
-		return (NULL);
+		current_column += term->column;
+		if (current_column > term->column_max)
+		{
+			current_column = term->column;
+			tputs("\n", 1, ft_pchar);
+		}
+		sz_name = ft_strlen(sv->current->name);
+		tputs(sv->current->name, 1, ft_pchar);
+		while (term->column > sz_name)
+		{
+			tputs(" ", 1, ft_pchar);
+			++sz_name;
+		}
+		sv->current = sv->current->next;
 	}
+	sv->current = sv->head;
+}
+
+void	move_column(char **keys, int *i, int *j, size_t size_column)
+{
+	int			column;
+
+	column = tgetnum("co");
+	column = column - (column % size_column);
+	if (*j >= column)
+	{
+		++(*i);
+		(*j) = 0;
+	}
+	else if (*j < 0)
+	{
+		if (*i > 0)
+			--(*i);
+		else
+		{
+			(*j) = 0;
+			return ;
+		}
+		(*j) = column - size_column;// - (column % size_column);
+	}
+	tputs(tgoto(keys[0], *j, *i), 1, ft_pchar);
+}
+
+void	move_lst(t_term_parameter *term, int top)
+{
+	int	i;
+
+	i = -1;
+	while (++i < (int)term->nb_column)
+	{
+		if (top)
+		{
+			if (term->select->current->next)
+				term->select->current = term->select->current->next;
+			else
+				break ;
+		}
+		else
+		{
+			if (term->select->current->prev)
+				term->select->current = term->select->current->prev;
+			else
+				break ;
+		}
+	}
+}
+
+void	move_head_lst(t_term_parameter *term, int nb)
+{
+	int	i;
+
+	i = -1;
+	while (++i < nb)
+	{
+		if (term->select->current->next)
+			term->select->current = term->select->current->next;
+		else
+			break ;
+	}
+}
+
+void	move_line(char **keys, int *i, int *j, t_term_parameter *term, int top)
+{
+	int	place;
+
+	place = (term->nb_column * term->line_max) - term->select->size_lst;
+	if (*i < 0)
+	{
+		*i = term->line_max - 1;
+		if (((int)term->column_max / *j) >= ((int)term->nb_column - place))
+			*i -= 1;
+	}
+	else if (*i >= (int)term->line_max)
+	{
+//		move_head_lst(term, *j);
+		*i = 0;
+	}
+	else if (*i == ((int)term->line_max - 1))
+	{
+		if (((int)term->column_max / *j) >= ((int)term->nb_column - place))
+			*i = 0;
+	}
+//	else
+//		move_lst(term, top);
+	tputs(tgoto(keys[0], *j, *i), 1, ft_pchar);
+}
+
+char	**init_keys_select(void)
+{
+	char	**keys;
+
+	if (!(keys = (char**)malloc(sizeof(char*) * 9)))
+		return (NULL);
 	keys[0] = tgetstr("cm", NULL);
 	keys[1] = tgetstr("cl", NULL);
 	keys[2] = tgetstr("kd", NULL);
 	keys[3] = tgetstr("ku", NULL);
 	keys[4] = tgetstr("kl", NULL);
 	keys[5] = tgetstr("kr", NULL);
-	keys[6] = tgetstr("DC", NULL);
+	keys[6] = tgetstr("dc", NULL);
 	keys[7] = tgetstr("ts", NULL);
+	keys[8] = 0;
+	return (keys);
+}
 
+void	del_column(char **keys, int i, int j, t_term_parameter *term)
+{
+	int	index;
+
+	index = -1;
+	while (++index < (int)term->column)
+		tputs(tgoto(keys[6], j, i), 1, ft_pchar);
+}
+
+char	*test(t_save_select *sv, t_term_parameter *term)
+{
+	int		i;
+	int		j;
+	char	**keys;
+	char	*final;
+	char	buf[3];
+
+	keys = init_keys_select();
 	tputs(keys[1], 1, ft_pchar);
-	i = -1;
-	while (list_arg[++i])
-	{
-		tputs(list_arg[i], 1, ft_pchar);//ft_printf("%s\t", list_arg[i]);
-		tputs("\t", 1, ft_pchar);//ft_printf("%s\t", list_arg[i]);
-	}
+	fill_screen(sv, term);
 	i = 0;
 	j = 0;
 	tputs(tgoto(keys[0], 0, i), 1, ft_pchar);
@@ -102,25 +243,43 @@ char	*test(char **list_arg)
 			break ;
 		else if (buf[0] == 32 && buf[1] == 0)
 		{
+//			ft_putstr("am here\n");
 			tputs(tgoto(tgetstr("so", NULL), j, i), 1, ft_pchar);
 			tputs(tgoto(tgetstr("sa", NULL), j, i), 1, ft_pchar);
-			tputs(list_arg[0], 1, ft_pchar);
+			tputs(sv->current->name, 1, ft_pchar);
 			tputs(tgoto(tgetstr("se", NULL), j, i), 1, ft_pchar);
 		}
 		else if (buf[0] == 27 && buf[1] == 91 && buf[2] == 51)
-			tputs(tgoto(keys[6], j, i), 1, ft_pchar);
+		{
+			del_column(keys, i, j, term);
+		}
 		else if (buf[0] == 27 && buf[1] == 91 && buf[2] == 65)
-			tputs(tgoto(keys[0], j, --i), 1, ft_pchar);
+		{
+			i -= 1;
+			move_line(keys, &i, &j, term, 0);
+		}
 		else if (buf[0] == 27 && buf[1] == 91 && buf[2] == 66)
-			tputs(tgoto(keys[0], j, ++i), 1, ft_pchar);
+		{
+			i += 1;
+			move_line(keys, &i, &j, term, 1);
+		}
 		else if (buf[0] == 27 && buf[1] == 91 && buf[2] == 67)
-			tputs(tgoto(keys[0], ++j, i), 1, ft_pchar);
+		{
+			if (term->select->current->next)
+			{
+				term->select->current = term->select->current->next;
+				j += term->column;
+				move_column(keys, &i, &j, term->column);
+			}
+		}
 		else if (buf[0] == 27 && buf[1] == 91 && buf[2] == 68)
 		{
-			//tputs(tgoto(tgetstr("us", NULL), j, i), 1, ft_pchar);
-			//tputs(list_arg[0], 1, ft_pchar);
-			tputs(tgoto(keys[0], --j, i), 1, ft_pchar);
-			//tputs(tgoto(tgetstr("ue", NULL), j, i), 1, ft_pchar);
+			if (term->select->current->prev)
+			{
+				term->select->current = term->select->current->prev;
+				j -= term->column;
+				move_column(keys, &i, &j, term->column);
+			}
 		}
 		ft_bzero(buf, 3);
 	}
@@ -128,41 +287,75 @@ char	*test(char **list_arg)
 	return (final);	
 }
 
+int							init_termcap(t_term_parameter *term)
+{
+	char							*env;
+	
+	if (tcgetattr(term->fd_in, &(term->base_term)) < 0)
+		return (0);
+	if (ft_tty_raw(term->base_term, term->fd_in) == -1)
+	{
+		ft_dprintf(2, "Error: raw doesn't work!\n");
+		return (0);
+	}
+	env = getenv("TERM");
+	if ((!env) || tgetent(0, env) == -1)
+	{
+		ft_dprintf(2, "Error: tgetent doesn't work!\n");
+		return (0);
+	}
+	return (1);
+}
+
+t_term_parameter	*init_term(t_save_select *s)
+{
+	t_term_parameter	*term;
+	int								column;
+	size_t						size_lst;
+
+	if (!(term = (t_term_parameter*)malloc(sizeof(t_term_parameter) * 1)))
+		return (NULL);
+	size_lst = ft_size_lst_select(s);
+	term->column = s->max_size + 2;
+	term->column_max_last_line = 0;
+	term->select = s;
+	term->fd_in = STDIN_FILENO;
+	if (!init_termcap(term))
+		return (NULL);
+	term->line = tgetnum("li");
+	column = tgetnum("co");
+	column = column - (column % term->column);
+	term->column_max = column;
+	term->nb_column = term->column_max / term->column;
+	term->line_max = size_lst / term->nb_column;
+	if ((size_lst % term->nb_column) > 0)
+		term->line_max += 1;
+	return (term);
+}
+
+int	reset_term(t_term_parameter *term)
+{
+	if (tty_reset(term->base_term, term->fd_in) == -1)
+	{
+		ft_dprintf(2, "Error: reset raw doesn't work!\n");
+		return (-1);
+	}
+	return (0);
+}
+
 int		main(int ac, char **av)
 {
-	t_save_select	*sv;
-
-	char					*final;
-	static	int				fd_in;
-	static	struct termios	base_term;
+	t_term_parameter	*term;
+	t_save_select			*sv;
+	char							*final;
 
 	if (ac < 2)
 		return (-1);
 	sv = create_lst_select(av + 1);
 	sv->current = sv->head;
-	while (sv->current)
-	{
-		ft_printf("%s\n", sv->current->name);
-		sv->current = sv->current->next;
-	}
-	ft_printf("====================\n\n");
-	
-
-	fd_in = STDIN_FILENO;
-	if (tcgetattr(fd_in, &base_term) < 0)
-		return (-1);
-	if (ft_tty_raw(base_term, fd_in) == -1)
-	{
-		ft_dprintf(2, "Error: raw doesn't work!\n");
-		return (-1);
-	}
-//		t_sv_cmd	*sv = create_list(av + 1);
-		final = test(av + 1);
-		ft_putstr(final);
-	if (tty_reset(base_term, fd_in) == -1)
-	{
-		ft_dprintf(2, "Error: reset raw doesn't work!\n");
-		return (-1);
-	}
+	term = init_term(sv);
+	final = test(sv, term);
+	ft_putstr(final);
+	reset_term(term);
 	return (0);
 }
